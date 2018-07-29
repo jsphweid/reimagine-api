e635ed5a7ff23f6a8490fe1d3f900e1455d7abe2
Joseph Weidinger <jsphweid@gmail.com>
2018-07-28 23:14:18 -0500

    refactor and add prettier

--- src/get-segment/get-segment.ts
+++ src/get-segment/get-segment.ts
@@ -3,8 +3,8 @@ import { pickRandomItem } from '../common/helpers'
 import { Context, Callback } from 'aws-lambda'
 
 async function getRandomSegmentId(): Promise<string> {
-	const client = await getPgClient()
-	const result = await client.query(`
+  const client = await getPgClient()
+  const result = await client.query(`
 		SELECT id, count
 		FROM segment
 		LEFT JOIN (SELECT "segmentId", COUNT(*) FROM recording GROUP BY "segmentId") counts
@@ -12,19 +12,32 @@ async function getRandomSegmentId(): Promise<string> {
 		ORDER BY COALESCE(count, 0) ASC
 		LIMIT 200;
 	`)
-	const idsThatHaveNoRecordings = result.rows.filter(row => !row.count).map(row => row.id)
-	return idsThatHaveNoRecordings.length ? pickRandomItem(idsThatHaveNoRecordings) : pickRandomItem(result.rows.map(row => row.id))
+  const idsThatHaveNoRecordings = result.rows
+    .filter(row => !row.count)
+    .map(row => row.id)
+  return idsThatHaveNoRecordings.length
+    ? pickRandomItem(idsThatHaveNoRecordings)
+    : pickRandomItem(result.rows.map(row => row.id))
 }
 
-export async function handler(event: any, context: Context, callback: Callback) {
-	context.callbackWaitsForEmptyEventLoop = false
-	const segmentId = event.id || (await getRandomSegmentId())
+export async function handler(
+  event: any,
+  context: Context,
+  callback: Callback
+) {
+  context.callbackWaitsForEmptyEventLoop = false
+  const segmentId = event.id || (await getRandomSegmentId())
 
-	const client = await getPgClient()
-	const result = await client.query(`select * from segment where id = '${segmentId}'`)
+  const client = await getPgClient()
+  const result = await client.query(
+    `select * from segment where id = '${segmentId}'`
+  )
 
-	if (!result.rows.length) return callback(new Error('Unable to get segment'))
-	const drilledResult = result.rows[0]
+  if (!result.rows.length) return callback(new Error('Unable to get segment'))
+  const drilledResult = result.rows[0]
 
-	callback(null, { ...drilledResult, midiJson: JSON.stringify(drilledResult.midiJson) })
+  callback(null, {
+    ...drilledResult,
+    midiJson: JSON.stringify(drilledResult.midiJson)
+  })
 }
--- src/synthesizer/synthesizer-test.ts
+++ src/synthesizer/synthesizer-test.ts
@@ -1,5 +1,9 @@
 import { synthesize } from './synthesizer'
 
-synthesize({ body: { pieceId: '8fdc99924d284cfc506dd6cdd9e5e13f' } }, '' as any, () => {
-	console.log('finished')
-})
+synthesize(
+  { body: { pieceId: '8fdc99924d284cfc506dd6cdd9e5e13f' } },
+  '' as any,
+  () => {
+    console.log('finished')
+  }
+)
